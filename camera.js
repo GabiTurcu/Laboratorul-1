e57629c46dc5c4367343b3de340b495f84063419
GabiTurcu <gabrielaturcu29@gmail.com>
2024-10-08 14:49:04 +0300

    Add files via upload

--- camera.js
+++ camera.js
@@ -1,25 +1,26 @@
 var video = document.getElementById("video");
-//-----------------------------------------------
-function on_cam_success(stream){
-video.srcObject = stream;
+
+video.addEventListener("touchstart", capteaza);
+video.addEventListener("mousedown", capteaza);
+
+function on_cam_success(stream) {
+    video.srcObject = stream;
 }
-//-----------------------------------------------
-function on_cam_error(err)
-{
-alert("error."+err.message);
+
+function on_cam_error(err) {
+    alert("Eroare: " + err.message);
 }
-//-----------------------------------------------
+
 var constraints = { audio: false, video: true };
+
 navigator.mediaDevices.getUserMedia(constraints)
-.then(on_cam_success)
-.catch(on_cam_error);
-//-----------------------------------------------
-function capteaza()
-{
-var c = document.getElementById("canvas");
-c.width = video.width;
-c.height = video.height;
-var ctx = c.getContext("2d");
-ctx.drawImage(video, 0, 0, 640, 480);
+    .then(on_cam_success)
+    .catch(on_cam_error);
+
+function capteaza() {
+    var c = document.getElementById("canvas");
+    c.width = video.videoWidth;  
+    c.height = video.videoHeight;
+    var ctx = c.getContext("2d");
+    ctx.drawImage(video, 0, 0, c.width, c.height);
 }
-//-----------------------------------------------
